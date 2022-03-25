2d8ec34e0ffcd2e1f93bfd6742008861f9008695
leska-ver <lyaisan.333@yandex.ru>
2022-03-25 12:09:19 +0500

    Из курса. Первый макет.

---
+++ auto-Jeka/js/auto-Jeka.js
@@ -0,0 +1,124 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  //Меню(data-target) клик или is-open--is-active//
+  document.querySelectorAll(".header__center-item-btn_js").forEach(item => {
+    item.addEventListener("click", function () {
+      let btn = this;
+      let dropdown = this.parentElement.querySelector(".header__center-dropdown_js");
+
+      document.querySelectorAll(".header__center-item-btn_js").forEach(el => {
+        if (el != btn) {
+          el.classList.remove("active--btn");
+        }
+      });
+
+      document.querySelectorAll(".header__center-dropdown_js").forEach(el => {
+        if (el != dropdown) {
+          el.classList.remove("active-list--item");
+        }
+      })
+      dropdown.classList.toggle("active-list--item");
+      btn.classList.toggle("active--btn")
+    })
+  })
+
+  document.addEventListener("click", function (e) {
+    let target = e.target;
+    if (!target.closest(".header__center-list_js")) {
+      document.querySelectorAll(".header__center-dropdown_js").forEach(el => {
+        el.classList.remove("active-list--item");
+      })
+      document.querySelectorAll(".header__center-item-btn_js").forEach(el => {
+        el.classList.remove("active--btn");
+      });
+    }
+  });
+
+
+
+  // burger
+  window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#burger').addEventListener('click', function() {
+        document.querySelector('#menu').classList.toggle('is-active')
+    }) 
+  })
+
+  $(document).ready(function(){
+    $('#burger').click(function(){
+      $(this).toggleClass('open');
+    });
+  });
+
+
+
+  // inputmask - Телефон
+  const form = document.querySelector('.form');
+  const telSelector = form.querySelector('input[type="tel"]');
+  const inputMask = new Inputmask('+7 (999) 999-99-99');
+  inputMask.mask(telSelector);
+
+  new window.JustValidate('.form', {
+    rules: {
+      tel: {
+        required: true,
+        function: () => {
+          const phone = telSelector.inputmask.unmaskedvalue();
+          return Number(phone) && phone.length === 10;
+        }
+      }
+    },
+    colorWrong: '#ff0f0f',
+    messages: {
+      name: {
+        required: 'Введите имя',
+        minLength: 'Введите 3 и более символов',
+        maxLength: 'Запрещено вводить более 15 символов'
+      },
+      email: {
+        email: 'Введите корректный email',
+        required: 'Введите email'
+      },
+      tel: {
+        required: 'Введите телефон',
+        function: 'Здесь должно быть 10 символов без +7'
+      },
+      text: {
+        required: 'Введите Select Service',
+        minLength: 'Введите 15 и более символов',
+        maxLength: 'Запрещено вводить более 25 символов'
+      }
+    },
+    submitHandler: function (thisForm) {
+      let formData = new FormData(thisForm);
+
+      let xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            console.log('Отправлено');
+          }
+        }
+      }
+
+         
+      xhr.open('POST', 'mail.php', true);
+      xhr.send(formData);
+
+      thisForm.reset();
+    }
+  });
+
+
+
+  // Плавный скролл по якорям. В любое место можно кинуть.
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var target = $(this).attr('href');
+      $('html, body').animate({scrollTop: $
+    (target).offset().top},800);
+      return false;
+    });
+  });
+
+});
